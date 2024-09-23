2f29487fc3315e020f731250b574fff6da02c51f
AlphaRant9 <alpharant9@gmail.com>
2024-09-22 20:52:43 -0500

    HW 2 code completion

--- Homework/HW2/sort.py
+++ Homework/HW2/sort.py
@@ -1,42 +1,63 @@
 import os
 
-sortedDict = {}
-sortedJumble = []
-
 def main():
-    sortDict()
-    sortJumble()
+    sortedDict = sortDict()
+    regularJumble, sortedJumble = sortJumble()
+    resultStr = compareJumble(sortedDict, regularJumble, sortedJumble)
+    print(resultStr)
+
+def compareJumble(sortedDict, regularJumble, sortedJumble):
+    """Compares the sorted jumbles to the sorted dictionary and returns what each could be decoded to or that it could
+        not be decoded."""
+    result = ""
+    for jumbleIndex in range(len(sortedJumble)):
+        result += f'{regularJumble[jumbleIndex]} '
+        if sortedDict.get(str(sortedJumble[jumbleIndex]).lower()) is None:
+            result += "could not be unscrabbled!\n"
+        else:
+            tempResult = sortedDict.get(str(sortedJumble[jumbleIndex]).lower())
+            result += f'could be unscrabbled to {tempResult}\n'
+
+
+    return result
 
 def sortJumble(jumbleFile = "jumble.txt"):
-    """Sorts Jumble File lines by character to be compared with """
+    """Sorts Jumble File lines by character to be compared with the dictionary"""
     if os.path.isfile(jumbleFile):
         jumble = open(jumbleFile)
     else:
         raise FileNotFoundError("Jumble file not found")
 
     jumbleLines = jumble.readlines()
+    regularJumble = []
+    sortedJumble = []
 
     for line in jumbleLines:
         temp = []
         tempStr = ""
+        tempSortedStr = ""
         for char in line:
             if char != "\n":
                 temp.append(char)
 
-        temp.sort()
-
         for char in temp:
             tempStr += char
 
-        sortedJumble.append(tempStr)
+        regularJumble.append(tempStr)
 
+        temp.sort()
 
+        for char in temp:
+            tempSortedStr += char
 
+        sortedJumble.append(tempSortedStr)
+
+    return regularJumble, sortedJumble
 
 def sortDict(dictFile = "dictionary.txt"):
     """Sorts every character alphabetically by line in a file and writes to a new file with the sorted list."""
     original = open(dictFile, "r")
-    global sortedDict
+    sortedDict = {}
 
     for line in original.readlines():
         temp = []
@@ -56,5 +77,6 @@ def sortDict(dictFile = "dictionary.txt"):
             sortedDict[tempStrSorted] = [tempStr]
         else:
             sortedDict[tempStrSorted].append(tempStr)
+    return sortedDict
 
 main()
